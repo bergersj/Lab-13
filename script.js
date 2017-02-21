734277fc7a3f3ee5629f4c5ffa574be01f518487
bergersj <joshbergers35@gmail.com>
2017-02-21 12:10:49 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+var app = angular.module('myModule', []);
+
+
+app.controller("stewieController", function($scope, $interval){
+  $scope.wordDisplay =[];
+
+  var wordBank = ['mom', 'mommy', 'mother', 'madre', 'brian', 'peter', 'woman'];
+
+  var phraseBank = ['blasted woman', 'world domination', 'pipe down lois', 'whatever helps you sleep at night', 'it wasnt even about the egg'];
+
+  var styleBank = ['red', 'orange', 'big', 'italic'];
+
+  $scope.wordClick = function(){
+    var randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+    $scope.wordDisplay.push({
+      text:randomWord,
+      class: 'none'
+    });
+  };
+
+  $scope.phraseClick = function(){
+    var randomPhrase = phraseBank[Math.floor(Math.random()*phraseBank.length)];
+    $scope.wordDisplay.push({
+      text:randomPhrase,
+      class: "none"
+    });
+  };
+
+  $scope.styleClick = function(){
+    var randomStyle = styleBank[Math.floor(Math.random()*styleBank.length)];
+    var randomWord = wordBank[Math.floor(Math.random()*wordBank.length)];
+    $scope.wordDisplay.push({
+      text: randomWord,
+      class: randomStyle
+    });
+  };
+
+  $scope.phraseStyleClick = function(){
+    var randomStyle = styleBank[Math.floor(Math.random()*styleBank.length)];
+    var randomPhrase = phraseBank[Math.floor(Math.random()*phraseBank.length)];
+    $scope.wordDisplay.push({
+      text: randomPhrase,
+      class: randomStyle
+    });
+  };
+
+ // $interval($scope.styleClick(), 1000);
+
+
+});
